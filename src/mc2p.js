f3f0d29537267d884ad1819f2ab97f5509f16411
Abel Puentes Luberta <apuentes@mychoice2pay.com>
2017-10-10 14:10:08 +0200

    Improved use of library

--- src/mc2p.js
+++ src/mc2p.js
@@ -5,15 +5,38 @@
   var root = typeof self === 'object' && self.self === self && self ||
             typeof global === 'object' && global.global === global && global;
 
-  var MC2P = function(MC2PPublicKey, token) {
+  var MC2P = function(MC2PPublicKey, token, customOptions) {
     this.MC2PPublicKey = MC2PPublicKey;
     this.token = token;
     this.API_URL = 'https://api.mychoice2pay.com/v1/pay/' + this.token + '/';
+    this.API_HTML_URL = this.API_URL + 'html/';
     this.API_CARD_URL = this.API_URL + 'card/';
     this.API_SHARE_URL = this.API_URL + 'share/';
+    this.DIVVY_URL = 'https://pay.mychoice2pay.com/#/' + this.token + '/divvy';
+
+    this.options = {
+      htmlDivId: (customOptions && customOptions.htmlDivId)|| 'html-div',
+      cardDivId: (customOptions && customOptions.cardDivId)|| 'card-div',
+      cardDataNameId: (customOptions && customOptions.cardDataNameId) || 'card-name',
+      cardDataNumberId: (customOptions && customOptions.cardDataNumberId) || 'card-number',
+      cardDataMonthId: (customOptions && customOptions.cardDataMonthId) || 'card-month',
+      cardDataYearId: (customOptions && customOptions.cardDataYearId) || 'card-year',
+      cardDataCVVId: (customOptions && customOptions.cardDataCVVId) || 'card-cvv'
+    };
+
+    this.gatewaysList = [];
+    this.gatewaySelected = null;
   };
 
-  MC2P.prototype.__request = function(method, url, data, callback) {
+  var replaceAndSubmitHtml = function (htmlDiv, mc2p) {
+    return function (data) {
+      htmlDiv.innerHTML = data.html;
+      htmlDiv.style.visibility = 'visible';
+      document.querySelector('#' + mc2p.options.htmlDivId + ' form').submit();
+    };
+  };
+
+  MC2P.prototype.__request = function (method, url, data, callback) {
     var xmlHttp = new XMLHttpRequest();
     if (callback) {
       xmlHttp.onreadystatechange = function() {
@@ -32,16 +55,64 @@
     }
   };
 
-  MC2P.prototype.getGateways = function(callback) {
-    return this.__request("GET", this.API_URL, null, callback);
+  MC2P.prototype.getGateways = function (callback) {
+    if (callback) {
+      var mc2p = this;
+      return this.__request("GET", this.API_URL, null, function (data) {
+        mc2p.gatewaysList = data.gateways;
+        callback(data);
+      });
+    } else {
+      var data = this.__request("GET", this.API_URL, null, callback);
+      this.gatewaysList = data.gateways;
+      return data;
+    }
   };
 
-  MC2P.prototype.card = function(gatewayCode, data, callback) {
+  MC2P.prototype.html = function (gatewayCode, callback) {
+    return this.__request("GET", this.API_HTML_URL + gatewayCode + '/', null, callback);
+  };
+
+  MC2P.prototype.card = function (gatewayCode, data, callback) {
     return this.__request("POST", this.API_CARD_URL + gatewayCode + '/', data, callback);
   };
 
-  MC2P.prototype.share = function(data, callback) {
+  MC2P.prototype.share = function (data, callback) {
     return this.__request("POST", this.API_SHARE_URL, data, callback);
+  };
+
+  MC2P.prototype.selectGateway = function (gatewayCode) {
+    var htmlDiv = document.getElementById(this.options.htmlDivId);
+    var cardDiv = document.getElementById(this.options.cardDivId);
+    htmlDiv.style.visibility = 'hidden';
+    cardDiv.style.visibility = 'hidden';
+
+    var index = 0;
+    for (; index < this.gatewaysList.length; index++) {
+      if (this.gatewaysList[index].code === gatewayCode) {
+        var gateway = this.gatewaysList[index];
+        if (gateway.form === 'html') {
+          this.html(gatewayCode, replaceAndSubmitHtml(htmlDiv, this));
+        } else if (gateway.form === 'card') {
+          cardDiv.style.visibility = 'visible';
+        } else if (gateway.form === 'shared') {
+          document.location = this.DIVVY_URL;
+        }
+        this.gatewaySelected = gateway;
+        break;
+      }
+    }
+  };
+
+  MC2P.prototype.sendCardDetails = function (callback) {
+    var cardData = {
+      'name': document.getElementById(this.options.cardDataNameId).value,
+      'number': document.getElementById(this.options.cardDataNumberId).value,
+      'month': document.getElementById(this.options.cardDataMonthId).value,
+      'year': document.getElementById(this.options.cardDataYearId).value,
+      'verification_value': document.getElementById(this.options.cardDataCVVId).value
+    };
+    return this.card(this.gatewaySelected.code, cardData, callback);
   };
 
   // Make module accessible:
